9a31a42d002be3ddebff8e4bc5a2dc37bb43f34f
keyplay <houyubo1993@foxmail.com>
2016-08-28 19:08:00 +0800

    Create passwordLocker.py

---
+++ passwordLocker.py
@@ -0,0 +1,9 @@
+#! python2.7
+# passwordLocker.py - An insecure password locker program.
+'''
+It is a password manager program that uses one master password to unlock the password manager
+This program isn’t secure, but it offers a basic demonstration of how such programs work.
+Then users can copy any account password to the clipboard and paste it into the website’s Password field.
+'''
+
+PASSWORDS = {'email':}

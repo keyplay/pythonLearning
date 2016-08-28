ab9c372ad1e46a1d1ad8e7c4865149dc1e76a36a
keyplay <houyubo1993@foxmail.com>
2016-08-28 18:32:47 +0800

    Update reExpression.py

--- reExpression.py
+++ reExpression.py
@@ -1,14 +1,11 @@
 # python2.7
 # phoneAndEmail.py - Finds phone numbers and email addresses on the clipboard.
 '''
-Say you have the boring task of finding every phone number 
-and email address in a long web page or document. If you manually 
-scroll through the page, you might end up searching for a long time. 
-But if you had a program that could search the text in 
-your clipboard for phone numbers and email addresses, you could simply 
-press CTRL-A to select all the text, press CTRL-C to copy it to the clipboard, 
-and then run your program. It could replace the text on the clipboard with just 
-the phone numbers and email addresses it finds
+A program that could search the text in the clipboard for phone numbers and 
+email addresses, users could simply press CTRL-A to select all the text, 
+press CTRL-C to copy it to the clipboard, and then run the program. 
+It could replace the text on the clipboard with just the phone numbers and 
+email addresses it finds
 '''
 
 import pyperclip, re

215c68d6db77cccf778bf23a4f92d60891289c82
keyplay <houyubo1993@foxmail.com>
2016-09-05 11:16:25 +0800

    Update lucky.py

--- lucky.py
+++ lucky.py
@@ -1,2 +1,19 @@
 #! python2.7
 # lucky.py - Opens several Google search results.
+
+import sys, webbrowser, requests, bs4
+
+print 'Googling...'    # display text while downloading the Google page
+res = requests.get('https://www.google.com/#q=' + ' '.join(sys.argv[1:]))
+try:
+    res.raise_for_status()
+except Exception as err:
+    print err
+    
+# Retrieve top search result links.
+soup = bs4.BeautifulSoup(res.text)
+# TODO: Open a browser tab for each result.
+elem = soup.select('.r a')   # find all <a> elements that are within an element that has the r CSS class.
+numOpen = min(5, len(elem))
+for i in range(numOpen):
+    webbrowser.open(elem[i].get('href'))

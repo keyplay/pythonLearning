67e29aa267f364910343dd86d4b8fbfead27950a
keyplay <houyubo1993@foxmail.com>
2016-09-08 10:07:13 +0800

    Update updateProduce.py

--- updateProduce.py
+++ updateProduce.py
@@ -10,7 +10,7 @@ sheet = wb.get_sheet_by_name('Sheet')
 PRICE_UPDATES = {'Garlic': 3.07,
                  'Celery': 1.19,
                  'Lemon': 1.27}
-# TODO: Loop through the rows and update the prices.
+# Loop through the rows and update the prices.
 for rowNum in range(2, sheet.max_row):
     produceName = sheet.cell(row=rowNum, column=1).value
     if produceName in PRICE_UPDATES:

13246094300e16a443bd41d7229b1493a614a1a2
keyplay <houyubo1993@foxmail.com>
2016-10-13 19:59:05 +0800

    Create sortSpecial.py

---
+++ sortSpecial.py
@@ -0,0 +1,9 @@
+#! python2.7
+# sortSpecial.py - filter the non palindrome.
+
+def by_name(t):
+    return t[0]
+
+L = [('Bob', 75), ('Adam', 92), ('Bart', 66), ('Lisa', 88)]
+L2 = sorted(L, key=by_name)
+print L2

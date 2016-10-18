3b5a0edc41754d8b04b23eb88923489894c479ce
keyplay <houyubo1993@foxmail.com>
2016-10-18 17:44:38 +0800

    Create safe_base64_decode.py

---
+++ safe_base64_decode.py
@@ -0,0 +1,20 @@
+#! python3.4
+# -*- coding: utf-8 -*-
+# safe_base64_decode.py - decode base64 whose = is removed.
+
+import base64
+
+def safe_base64_decode(s):
+    remainder = len(s)%4
+    if remainder == 0:
+        return base64.b64decode(s)
+        
+    for i in range(remainder):
+        s += b'='
+    return base64.b64decode(s)
+
+
+# test:
+assert b'abcd' == safe_base64_decode(b'YWJjZA=='), safe_base64_decode('YWJjZA==')
+assert b'abcd' == safe_base64_decode(b'YWJjZA'), safe_base64_decode('YWJjZA')
+print('Pass')
